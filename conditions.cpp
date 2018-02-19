a94eec99d40694ae07ea67e881f3c5aa5274f38f
Joyce Njeri <tamiranjeri98@gmail.com>
2018-02-19 15:51:27 +0300

    added file

---
+++ conditions.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a;
+  int b;
+  
+  cout<<"enter first number";
+  cin>> a;
+  
+  cout<<"enter second number";
+  cin>>b;
+  
+if(a>b){
+    cout<<"a is greater ";
+}
+else if (a==b){
+    cout<<"a is equal to b";
+}
+else{"b is greater";}
+    return 0;
+}
